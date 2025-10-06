d73dddafa068f997c85ed8ce61e25b7b1694affa
Volt0303 <shunhosoo@gmail.com>
2025-10-06 00:01:09 -0700

    feat: fix useProductFilters

---
+++ src/components/Animations/FadeLeftToRightItem.component.tsx
@@ -0,0 +1,39 @@
+import { motion, Transition } from 'motion/react';
+
+import type { IAnimateProps } from './types/Animations.types';
+
+/**
+ * Fade content left to right. Needs to be used with FadeLeftToRight as parent container
+ * @function FadeLeftToRightItem
+ * @param {ReactNode} children - Children content to render
+ * @param {string} cssClass - CSS classes to apply to component
+ * @returns {JSX.Element} - Rendered component
+ */
+
+const FadeLeftToRightItem = ({ children, cssClass }: IAnimateProps) => {
+  const transition: Transition = {
+    type: 'spring',
+    duration: 0.5,
+    stiffness: 110,
+  };
+
+  const fadeLeftToRightItemVariants = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition,
+    },
+    hidden: { opacity: 0, x: -20 },
+  };
+  return (
+    <motion.span
+      variants={fadeLeftToRightItemVariants}
+      className={cssClass}
+      data-testid="fadelefttorightitem"
+    >
+      {children}
+    </motion.span>
+  );
+};
+
+export default FadeLeftToRightItem;
---
+++ src/components/Animations/FadeUp.component.tsx
@@ -0,0 +1,44 @@
+import { motion, Transition } from 'motion/react';
+
+import type { IAnimateWithDelayProps } from './types/Animations.types';
+
+/**
+ * Fade up content animation
+ * @function FadeUp
+ * @param {ReactNode} children - Children content to render
+ * @param {string} cssClass - CSS classes to apply to component
+ * @param {number} delay - Time to wait before starting animation
+ * @returns {JSX.Element} - Rendered component
+ */
+
+const FadeUp = ({ children, cssClass, delay }: IAnimateWithDelayProps) => {
+  const transition: Transition = {
+    delay,
+    type: 'spring',
+    duration: 0.5,
+    stiffness: 110,
+  };
+ 
+  const fadeUpVariants = {
+    initial: { opacity: 0, y: 20 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition,
+    },
+  };
+
+  return (
+    <motion.div
+      className={cssClass}
+      variants={fadeUpVariants}
+      initial="initial"
+      animate="animate"
+      data-testid="fadeup"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default FadeUp;
